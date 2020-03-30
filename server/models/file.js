9565feca177d27552ee2079d84b15341f09d3f11
ThaKushi <edupedro1996@gmail.com>
2020-03-30 03:24:11 +0100

    Fixed all

--- server/models/file.js
+++ server/models/file.js
@@ -6,7 +6,6 @@ let FileSchema = new Schema({
   encodedName: { type: String, required: false, max: 100, default: null },
   originalName: { type: String, required: false, max: 100, default: null },
   firstIP: { type: String, required: false, max: 100, default: null }
-  firstIP: { type: String, required: false, max: 100, default: null }
 });
 
 module.exports = mongoose.model('file', FileSchema, 'files');
